b9a1f0ef4bbd386903152e3a141601314589c2a5
Alfonso Pizarro <ajpizarror@gmail.com>
2026-06-11 08:47:47 -0400

    agregando swagger. falta hateoas

--- src/main/java/dsy1103/bibliotecaam/sancion/controller/SancionController.java
+++ src/main/java/dsy1103/bibliotecaam/sancion/controller/SancionController.java
@@ -2,7 +2,14 @@ package dsy1103.bibliotecaam.sancion.controller;
 
 import dsy1103.bibliotecaam.sancion.dto.SancionRequestDTO;
 import dsy1103.bibliotecaam.sancion.dto.SancionResponseDTO;
+import dsy1103.bibliotecaam.sancion.model.Sancion;
 import dsy1103.bibliotecaam.sancion.service.SancionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,16 +24,27 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/bibliotecaam/sancion")
 @RequiredArgsConstructor
+@Tag(name = "Sancion", description = "Operaciones asociadas a sanciones.")
 public class SancionController {
 
     private final SancionService sancionService;
 
     @GetMapping
+    @Operation(summary = "Obtener todas las sancion", description = "Obtiene una lista de todas las sancinoes.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Operacion exitosa"),
+            @ApiResponse(responseCode = "404", description = "Sancion no encontrada")
+    })
     public ResponseEntity<List<SancionResponseDTO>> obtenerTodas() {
         return ResponseEntity.ok(sancionService.obtenerTodas());
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Obtener sancion por id", description = "Obtiene una sancion acorde a una id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Operación exitosa"),
+            @ApiResponse(responseCode = "404", description = "Sancion no encontrada")
+    })
     public ResponseEntity<SancionResponseDTO> obtenerPorId(@PathVariable Long id) {
         return sancionService.obtenerPorId(id)
                 .map(ResponseEntity::ok)
@@ -34,26 +52,54 @@ public class SancionController {
     }
 
     @GetMapping("/pagado")
+    @Operation(summary = "Obtener sanciones acorde a si estan pagadas", description = "Obtiene una sancion acorde a si esta pagada.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Operación exitosa"),
+            @ApiResponse(responseCode = "404", description = "Sancion no encontrada")
+    })
     public ResponseEntity<List<SancionResponseDTO>> obtenerSancionPagada() {
         return ResponseEntity.ok(sancionService.obtenerSancionesPagadas());
     }
 
     @GetMapping("/nopagado")
+    @Operation(summary = "Obtener sanciones acorde a si no estan pagadas", description = "Obtiene una sancion acorde a si no esta pagada.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Operación exitosa"),
+            @ApiResponse(responseCode = "404", description = "Sancion no encontrada")
+    })
     public ResponseEntity<List<SancionResponseDTO>> obtenerSancionNoPagada() {
             return ResponseEntity.ok(sancionService.obtenerSancionesNoPagadas());
         }
 
     @GetMapping("/porfecha")
+    @Operation(summary = "Obtener sanciones acorde a una fecha", description = "Obtiene una sancion acorde a una fecha.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Operación exitosa"),
+            @ApiResponse(responseCode = "404", description = "Sancion no encontrada")
+    })
     public ResponseEntity<List<SancionResponseDTO>> obtenerPorFecha(LocalDate fecha){
         return ResponseEntity.ok(sancionService.obtenerPorFecha(fecha));
     }
 
     @PostMapping
+    @Operation(summary = "Guardar una sancion", description = "Guarda una sancion acorde a lo ingresado.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Operación exitosa."),
+            @ApiResponse(responseCode = "400", description = "Error al ingresar parametros. Revise si ingreso todos los parametros solicitados."),
+            @ApiResponse(responseCode = "403", description = "No tienes permiso para hacer el cambio.")
+    })
     private ResponseEntity<SancionResponseDTO> guardar(@Valid @RequestBody SancionRequestDTO dto){
         return ResponseEntity.status(201).body(sancionService.guardar(dto));
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Actualizar sancion", description = "Actualiza una sancion acorde a una id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Sancion actualizada",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Sancion.class))),
+            @ApiResponse(responseCode = "404", description = "El id de la sancion no existe.")
+    })
     public ResponseEntity<SancionResponseDTO> actualizar(
             @PathVariable Long id,
             @Valid @RequestBody SancionRequestDTO dto) {
@@ -63,6 +109,11 @@ public class SancionController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Eliminar sancion", description = "Elimina una sancion acorde a una id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "¡Sancion eliminada con exito!"),
+            @ApiResponse(responseCode = "404",description = "ERROR: ¡El id de la sancion ingresada no existe!")
+    })
     public ResponseEntity<Map<String,String>> eliminar(@PathVariable Long id) {
         if (sancionService.obtenerPorId(id).isEmpty()){
             Map<String, String> borrado = new LinkedHashMap<>();
